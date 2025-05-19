089e79edef27c23a97c6893567e5f0cff7c5ef3b
EricLarwa <larwae22@students.ecu.edu>
2025-05-19 12:00:50 -0400

    GUI Component fixed, Config files altered

---
+++ src/renderer/components/NowPlaying.d.ts
@@ -0,0 +1,1 @@
+export default function SpotifyNowPlaying(): import("react/jsx-runtime").JSX.Element;
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  root: path.resolve(__dirname, 'src/renderer'), // 👈 Set root to where index.html is
   plugins: [react()],
-  root: path.resolve(__dirname, 'renderer'),
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: path.resolve(__dirname, 'dist/renderer'), // output directory
     emptyOutDir: true,
   },
-});
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/renderer'),
+    },
+  },
+})
